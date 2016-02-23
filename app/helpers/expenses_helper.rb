73564880388c18e79c38a5f1e2ef99f4ba8a0091
Suneel Kumar (01814) <suneel.kumar@nxb.com.pk>
2016-02-23 21:41:02 +0500

    bulk actions error message content fixed

--- app/helpers/expenses_helper.rb
+++ app/helpers/expenses_helper.rb
@@ -4,7 +4,7 @@ module ExpensesHelper
       notice = <<-HTML
      <p>#{ids.size} expenses have been archived. You can find them under
      <a href="?status=archived#{query_string(params.merge(per: session["#{controller_name}-per_page"]))}" data-remote="true">Archived</a> section on this page.</p>
-     <p><a href='expenses/undo_actions?ids=#{ids.join(",")}&archived=true#{query_string(params.merge(per: session["#{controller_name}-per_page"]))}'  data-remote="true">Undo this action</a> to move archived companies back to active.</p>
+     <p><a href='expenses/undo_actions?ids=#{ids.join(",")}&archived=true#{query_string(params.merge(per: session["#{controller_name}-per_page"]))}'  data-remote="true">Undo this action</a> to move archived expenses back to active.</p>
       HTML
       notice.html_safe
     end
@@ -13,7 +13,7 @@ module ExpensesHelper
       notice = <<-HTML
      <p>#{ids.size} expenses have been deleted. You can find them under
      <a href="?status=deleted" data-remote="true">Deleted</a> section on this page.</p>
-     <p><a href='expenses/undo_actions?ids=#{ids.join(",")}&deleted=true#{query_string(params.merge(per: session["#{controller_name}-per_page"]))}'  data-remote="true">Undo this action</a> to move deleted companies back to active.</p>
+     <p><a href='expenses/undo_actions?ids=#{ids.join(",")}&deleted=true#{query_string(params.merge(per: session["#{controller_name}-per_page"]))}'  data-remote="true">Undo this action</a> to move deleted expenses back to active.</p>
       HTML
       notice.html_safe
     end
