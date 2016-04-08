69c675754ed33e948f4db7c3285a0e5d4d915f3e
Nauman Tahir <nomibzu@gmail.com>
2016-04-08 22:19:28 +0500

    Validation for hours in Timetracking Week view: bulk log create Form

--- app/assets/javascripts/logs.js
+++ app/assets/javascripts/logs.js
@@ -44,13 +44,25 @@ $(document).ready(function() {
 
     $('#log_date').val($.datepicker.formatDate('yy-mm-dd', new Date()));
     jQuery(".log-submit-btn").live("click", function () {
-
         var date, project_id, task_id, hours;
         date = jQuery("#log_date").val();
         project_id = jQuery("#log_project_id").val();
         task_id = jQuery("#log_task_id").val();
         hours = jQuery("#log_hours").val();
         flag = true;
+        window.valid = 0;
+        $(".frm_week input[type=number]").filter(function () {
+            console.log($(this).val());
+            console.log(window.valid);
+            if ($.trim($(this).val()).length == 0) window.valid+=1;
+        });
+
+        if (window.valid == 7){
+            applyPopover(jQuery("#hours_div"), "bottomMiddle", "topLeft", "Enter hours");
+            flag = false;
+        }else {
+            hidePopover(jQuery("#hours_div"))
+        }
         if (date === "") {
             applyPopover(jQuery("#log_date"), "bottomMiddle", "topLeft", "Select a date from the calendar");
             flag = false;
