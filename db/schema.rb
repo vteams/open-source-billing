9cf3223f7fba06777f2d9e554fe140ef9f770a7f
Shahzad Tariq <shahzad.tariq@nxb.com.pk>
2016-04-11 12:27:34 +0500

    local changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160303151412) do
+ActiveRecord::Schema.define(version: 20160330110032) do
 
   create_table "account_users", force: true do |t|
     t.integer "user_id"
