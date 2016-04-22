b621a9e4a2f3db2b0506ea4e122a7c29f5e04e80
Suneel Kumar (01814) <suneel.kumar@nxb.com.pk>
2016-04-22 15:51:46 +0500

    down case the config yml constant and add config.yml file into git ignore

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -2,23 +2,23 @@ module OSB
   module CONFIG
 
     APP_CONFIG = HashWithIndifferentAccess.new(YAML.load_file(Rails.root.join('config','config.yml'))[Rails.env])
-    APP_HOST ||= APP_CONFIG[:APP_HOST]
-    APP_PROTOCOL ||= APP_CONFIG[:APP_PROTOCOL]
+    APP_HOST ||= APP_CONFIG[:app_host]
+    APP_PROTOCOL ||= APP_CONFIG[:app_protocol]
 
-    ACTIVEMERCHANT_BILLING_MODE ||= APP_CONFIG[:ACTIVEMERCHANT_BILLING_MODE]
+    ACTIVEMERCHANT_BILLING_MODE ||= APP_CONFIG[:activemerchant_billing_mode]
 
-    PAYPAL ||= APP_CONFIG[:PAYPAL]
-    PAYPAL_URL ||= PAYPAL[:PAYPAL_URL]
-    PAYPAL_LOGIN ||= PAYPAL[:PAYPAL_LOGIN]
-    PAYPAL_PASSWORD ||= PAYPAL[:PAYPAL_PASSWORD]
-    PAYPAL_SIGNATURE ||= PAYPAL[:PAYPAL_SIGNATURE]
-    PAYPAL_BUSINESS ||= PAYPAL[:PAYPAL_BUSINESS]
+    PAYPAL ||= APP_CONFIG[:paypal]
+    PAYPAL_URL ||= PAYPAL[:paypal_url]
+    PAYPAL_LOGIN ||= PAYPAL[:paypal_login]
+    PAYPAL_PASSWORD ||= PAYPAL[:paypal_password]
+    PAYPAL_SIGNATURE ||= PAYPAL[:paypal_signature]
+    PAYPAL_BUSINESS ||= PAYPAL[:paypal_business]
 
-    WKHTMTTOPDF_PATH ||= APP_CONFIG[:WKHTMTTOPDF_PATH]
+    WKHTMTTOPDF_PATH ||= APP_CONFIG[:wkhtmltopdf_path]
 
-    SMTP_SETTING ||= APP_CONFIG[:SMTP_SETTING]
+    SMTP_SETTING ||= APP_CONFIG[:smtp_setting]
 
-    ENCRYPTION_KEY ||= APP_CONFIG[:ENCRYPTION_KEY]
+    ENCRYPTION_KEY ||= APP_CONFIG[:encryption_key]
 
   end
 end
