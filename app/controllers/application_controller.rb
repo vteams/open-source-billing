5a1b9a19652618de636012a13460b08f60c2b681
Suneel Kumar (01814) <suneel.kumar@nxb.com.pk>
2016-07-27 20:04:04 +0500

    cancan access denied message issue fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,7 @@ class ApplicationController < ActionController::Base
 
   before_action :set_locale
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, :alert => exception.message
+    redirect_to dashboard_url, :alert => exception.message
   end
 
 
