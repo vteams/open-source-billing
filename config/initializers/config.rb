82c83fa9cb086d0ec1614eb00bf786ebbad124e6
Shahroon Ali <shahroon.ali@gmail.com>
2016-11-23 20:20:03 +0500

    config.yml in shared

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,7 +1,8 @@
+require 'capistrano/rails'
 module OSB
   module CONFIG
 
-    APP_CONFIG = HashWithIndifferentAccess.new(YAML.load_file(Rails.root.join('config',"#{shared_path}/config/config.yml"))[Rails.env])
+    APP_CONFIG = HashWithIndifferentAccess.new(YAML.load_file(Rails.root.join('config',"#{shared_path}/config.yml"))[Rails.env])
     APP_HOST ||= APP_CONFIG[:app_host]
     APP_PROTOCOL ||= APP_CONFIG[:app_protocol]
     ACTIVEMERCHANT_BILLING_MODE ||= APP_CONFIG[:activemerchant_billing_mode]
