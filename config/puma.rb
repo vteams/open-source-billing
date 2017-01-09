b953c689cc8d7da7456ed005e855183cc0962719
Suneel Kumar (01814) <suneel.kumar@nxb.com.pk>
2017-01-09 20:25:54 +0500

    remove port from puma configuration

--- config/puma.rb
+++ config/puma.rb
@@ -4,9 +4,6 @@ threads threads_count, threads_count
 
 preload_app!
 
-port  3000
-
-
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
