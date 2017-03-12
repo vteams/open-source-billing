8e5b06038f9531a4ff28ac759e3844b43631be96
Derek Provance <derek@provance.me>
2017-03-11 19:29:51 -0600

    Updated Templates to properly print currency and company

--- lib/reporting/report.rb
+++ lib/reporting/report.rb
@@ -24,11 +24,11 @@ module Reporting
     attr_accessor :report_name, :report_criteria, :report_data, :client_name, :report_duration, :report_total
 
     def client_name
-      @report_criteria.client_id == 0 ? "All Clients" : Client.where(:id => @report_criteria.client_id).first.organization_name
+      @report_criteria.client_id.zero? ? "All Clients" : Client.where(:id => @report_criteria.client_id).first.organization_name
     end
 
     def item_name
-      @report_criteria.item_id == 0 ? "All Items" : Item.where(:id => @report_criteria.item_id).first.item_name
+      @report_criteria.item_id.zero? ? "All Items" : Item.where(:id => @report_criteria.item_id).first.item_name
     end
 
   end
@@ -39,7 +39,7 @@ module Reporting
       invoices.each do |invoice|
         ["First", "Second", "Third"].each do |reminder_number|
           email_reminder = EmailTemplate.late_payment_reminder_template(invoice, "#{reminder_number} Late Payment Reminder")
-          InvoiceMailer.delay(:run_at => email_reminder.no_of_days.days.from_now).late_payment_reminder_email(invoice.id, "#{reminder_number} Late Payment Reminder")  if invoice.late_payment_reminder(reminder_number).blank? and  email_reminder.send_email
+          InvoiceMailer.delay(:run_at => email_reminder.no_of_days.days.from_now).late_payment_reminder_email(invoice.id, "#{reminder_number} Late Payment Reminder")  if invoice.late_payment_reminder(reminder_number).blank? && email_reminder.send_email
         end
       end
       Reporting::Reminder.delay(:run_at => 1.day.from_now).late_payment_reminder
