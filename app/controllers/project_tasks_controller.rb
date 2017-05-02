5cfcf18c86c8b7a6f4756a008622dba428b9cdd8
Suneel Kumar (01814) <suneel.kumar@nxb.com.pk>
2017-05-02 19:25:27 +0500

    Project log created in background for its invoice purpose

--- app/controllers/project_tasks_controller.rb
+++ app/controllers/project_tasks_controller.rb
@@ -10,6 +10,7 @@ class ProjectTasksController < ApplicationController
     @project = Project.find(params[:project_id])
     @project_task = @project.project_tasks.new(project_task_params)
     if @project_task.save
+      @project_task.create_time_log(current_user)
       redirect_to project_path(@project), notice: 'Task was successfully created.'
     else
       redirect_to project_path(@project), alert: 'Task was not successfully created.'
@@ -18,6 +19,7 @@ class ProjectTasksController < ApplicationController
 
   def update
     if @project_task.update(project_task_params)
+      @project_task.update_time_log(current_user)
       redirect_to project_path(@project), notice: 'Task was successfully updated.'
     else
       redirect_to project_path(@project), alert: 'Task was not successfully updated.'
--- app/models/project_task.rb
+++ app/models/project_task.rb
@@ -15,4 +15,12 @@ class ProjectTask < ActiveRecord::Base
     ((spent_time * 100)/hours).round
   end
 
+  def create_time_log(user)
+    build_log(project_id: project_id, hours: spent_time, user_id: user.id, date: updated_at.to_date).save
+  end
+
+  def update_time_log(user)
+    log.present? ? log.update(hours: spent_time, user_id: user.id, date: updated_at.to_date) : create_time_log(user)
+  end
+
 end
