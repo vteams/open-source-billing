fde544dab1ca6de0a2b3445b9d1348fcc9541a93
Tabish Saleem <tabish.saleem@nxb.com.pk>
2017-12-14 21:07:15 +0500

    Invoice: Fixed load items issue for upgraded gem jquery-rails

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -140,7 +140,7 @@ class ItemsController < ApplicationController
 #  # Load invoice line items data when an item is selected from drop down list
   def load_item_data
     item = Item.find_by_id(params[:id]).present? ?  Item.find(params[:id]) : Item.unscoped.find_by_id(params[:id])
-    render :text => [item.item_description || "", item.unit_cost.to_f || 1, item.quantity.to_i || 1, item.tax_1 || 0, item.tax_2 || 0, item.item_name || ""]
+    render :text => [item.item_description || "", item.unit_cost.to_f || 1, item.quantity.to_i || 1, item.tax_1 || 0, item.tax_2 || 0, item.item_name || "", item.id]
   end
 
   def bulk_actions
