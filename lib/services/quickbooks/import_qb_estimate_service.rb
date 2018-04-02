6b09903c398d186522567fbd608654324ba39b20
Muhammad Tabish Saleem (01933) <tabish.saleem@nxb.com.pk>
2018-04-02 19:58:44 +0500

    QbEstimateService: Added tax calculations and discount type

--- lib/services/quickbooks/import_qb_estimate_service.rb
+++ lib/services/quickbooks/import_qb_estimate_service.rb
@@ -8,15 +8,22 @@ module Services
       qbo_api.all(:estimates).each do |estimate|
         if estimate.present?
           unless ::Estimate.find_by_provider_id(estimate['Id'].to_i)
+            if estimate['Line'].last['DetailType'].eql?('DiscountLineDetail')
+              discount_per =  estimate['Line'].last['DiscountLineDetail']['DiscountPercent'] if estimate['Line'].last['DiscountLineDetail']['PercentBased']
+              discount_amount = estimate['Line'].last['Amount']
+              discount_type = estimate['Line'].last['DiscountLineDetail']['PercentBased'] ? '%' : estimate['CurrencyRef']['value']
+            end
             hash = { provider: 'Quickbooks', provider_id: estimate['Id'].to_i,
                      estimate_number: estimate['DocNumber'],
                      po_number: nil,
-                     estimate_date: (estimate['ShipDate'] || estimate['MetaData']['CreatedTime']).to_date,
-                     discount_percentage: nil,
+                     estimate_date: (estimate['ShipDate'] || estimate['MetaData']['CreateTime']).to_date,
                      notes: estimate['PrivateNote'],
                      terms: nil, status: estimate['EmailStatus'] == 'NotSet' ? 'Draft' : 'Sent',
                      company_id: options[:current_company_id],
-                     discount_type: '%', estimate_total: estimate['TotalAmt'].to_f }
+                     discount_type: discount_type,
+                     estimate_total: estimate['TotalAmt'].to_f,
+                     discount_percentage: discount_per,
+                     discount_amount: discount_amount}
             osb_estimate=  ::Estimate.new(hash)
             osb_estimate.currency = ::Currency.find_by_unit(estimate['CurrencyRef']['value']) if estimate['CurrencyRef'].present? && estimate['CurrencyRef']['value'].present?
             osb_estimate.client = ::Client.find_by_provider_id(estimate['CustomerRef']['value'].to_i) if estimate['CustomerRef'].present? && estimate['CustomerRef']['value'].present?
@@ -26,19 +33,28 @@ module Services
             estimate['Line'].each do |item|
               if item['SalesItemLineDetail'].present? && item['SalesItemLineDetail']['ItemRef'].present? && item['SalesItemLineDetail']['ItemRef']['name'].present?
                 amount += item['Amount'].to_f
+                qb_tax_id = estimate['TxnTaxDetail']['TaxLine'][0]['TaxLineDetail']['TaxRateRef']['value'] if estimate['TxnTaxDetail'] && estimate['TxnTaxDetail']['TaxLine'].present? &&
+                    estimate['TxnTaxDetail']['TaxLine'][0]['TaxLineDetail']['TaxRateRef']
+                if qb_tax_id.present?
+                  qb_tax = qbo_api.query(%{SELECT * FROM TaxRate  WHERE Id = '#{qb_tax_id.to_i}'}).first
+                  osb_tax = ::Tax.find_by_name_and_percentage(qb_tax['Name'], qb_tax['RateValue'].to_f)
+                  if osb_tax.present?
+                    tax_1 = osb_tax.id
+                  else
+                    new_tax = ::Tax.create(name: qb_tax['Name'], percentage: qb_tax['RateValue'].to_f)
+                    tax_1 = new_tax.id
+                  end
+                end
                 item_hash = {item_name: item['SalesItemLineDetail']['ItemRef']['name'],
                              item_description: item['Description'],
                              item_unit_cost: item['SalesItemLineDetail']['UnitPrice'].to_f,
-                             item_quantity:  item['SalesItemLineDetail']['Qty'].to_i}
+                             item_quantity:  item['SalesItemLineDetail']['Qty'].to_i, tax_1: tax_1}
                 osb_item = osb_estimate.estimate_line_items.new(item_hash)
-                #osb_item.tax_1 = ::Tax.find_by_name_and_percentage(item['tax1_name'], item['tax1_percent'].to_f).try(:id) if item['tax1_name'].present?
-                #osb_item.tax_2 = ::Tax.find_by_name_and_percentage(item['tax2_name'],item['tax2_percent'].to_f).try(:id) if item['tax2_name'].present?
                 osb_item.save
               end
             end
             osb_estimate.create_line_item_taxes
-            discount_amount = nil #amount * (osb_estimate.discount_percentage/100)
-            osb_estimate.update_attributes(sub_total: amount, discount_amount: discount_amount)
+            osb_estimate.update_attributes(sub_total: amount)
           end
         end
       end
