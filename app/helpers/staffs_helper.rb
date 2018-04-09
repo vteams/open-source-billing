182af336a9e614d27bde12470b06643af6849144
Wasif Saeed (2116) <wasif.saeed@nxb.com.pk>
2018-04-09 11:49:54 +0500

    Issue#121 Added translations for staffs action msgs and validations

--- app/helpers/staffs_helper.rb
+++ app/helpers/staffs_helper.rb
@@ -2,18 +2,18 @@ module StaffsHelper
   include ApplicationHelper
   def staffs_archived(ids)
     notice = <<-HTML
-     <p>#{ids.size} staff have been archived. You can find them under
-     <a href="?status=archived#{query_string(params.merge(per: session["#{controller_name}-per_page"]))}" data-remote="true">Archived</a> section on this page.</p>
-     <p><a href='staffs/undo_actions?ids=#{ids.join(",")}&archived=true#{query_string(params.merge(per: session["#{controller_name}-per_page"]))}'  data-remote="true">Undo this action</a> to move archived expenses back to active.</p>
+     <p>#{ids.size} #{t('views.staffs.bulk_archived_msg')}
+     <a href="?status=archived#{query_string(params.merge(per: session["#{controller_name}-per_page"]))}" data-remote="true">#{t('views.common.archived')}</a> #{t('views.items.section_on_page')}</p>
+     <p><a href='staffs/undo_actions?ids=#{ids.join(",")}&archived=true#{query_string(params.merge(per: session["#{controller_name}-per_page"]))}'  data-remote="true">#{t('views.items.undo_action')}</a> #{t('views.staffs.move_archived_to_active')}</p>
     HTML
     notice.html_safe
   end
 
   def staffs_deleted(ids)
     notice = <<-HTML
-     <p>#{ids.size} staff have been deleted. You can find them under
-     <a href="?status=deleted" data-remote="true">Deleted</a> section on this page.</p>
-     <p><a href='staffs/undo_actions?ids=#{ids.join(",")}&deleted=true#{query_string(params.merge(per: session["#{controller_name}-per_page"]))}'  data-remote="true">Undo this action</a> to move deleted expenses back to active.</p>
+     <p>#{ids.size} #{t('views.staffs.bulk_deleted_msg')}
+     <a href="?status=deleted" data-remote="true">#{t('views.common.deleted')}</a> #{t('views.items.section_on_page')}</p>
+     <p><a href='staffs/undo_actions?ids=#{ids.join(",")}&deleted=true#{query_string(params.merge(per: session["#{controller_name}-per_page"]))}'  data-remote="true">#{t('views.items.undo_action')}</a> #{t('views.staffs.move_deleted_to_active')}</p>
     HTML
     notice.html_safe
   end
