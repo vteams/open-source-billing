31bc3bb5481d58b9ffd778675bdb6f91f8c1909a
Muhammad Tabish Saleem (01933) <tabish.saleem@nxb.com.pk>
2018-04-09 15:56:39 +0500

    SubUser: Redirected to sign in after password update and added locales

--- app/helpers/sub_users_helper.rb
+++ app/helpers/sub_users_helper.rb
@@ -2,4 +2,8 @@ module SubUsersHelper
   def load_user_roles
     Role.all.map{|r| [r.name.humanize, r.id]}
   end
+
+  def password_has_changed?(user_id, password)
+    current_user.id.to_s.eql?("#{user_id}") && password.present?
+  end
 end
