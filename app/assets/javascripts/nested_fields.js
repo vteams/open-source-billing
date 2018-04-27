3725fa5509a4c13976cd699c2e75d9afd88da3ec
Muhammad Tabish Saleem (01933) <tabish.saleem@nxb.com.pk>
2018-04-27 16:11:01 +0500

    Project: Fixed dynamically adding staff-member id issue

--- app/assets/javascripts/nested_fields.js
+++ app/assets/javascripts/nested_fields.js
@@ -55,6 +55,12 @@
                 .trigger({ type: 'nested:fieldAdded:' + assoc, field: field });
 
             $("select.items_list:last, select.tax1, select.tax2, select.tasks_list:last, select.members_list:last").material_select();
+            if ($('.project-form-inline').length > 0){
+                var id = $('#staff_container').find('.fields').length;
+                var element = $('#staff_container').find('.fields:last');
+                $(element).find('.filled-in').attr('id', 'project_staff_' + id);
+                $(element).find('.staff-label').attr('for', 'project_staff_' + id);
+            }
             Invoice.change_invoice_item();
             Invoice.changeTax();
             Invoice.load_functions();
