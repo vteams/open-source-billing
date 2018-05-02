48c3981f3c1981a048604e8dadca43307058db5b
Muhammad Tabish Saleem (01933) <tabish.saleem@nxb.com.pk>
2018-05-02 18:05:02 +0500

    Report: Capitalized first letter of client name

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,5 +1,5 @@
 module ReportsHelper
   def all_clients_or_individual_client_name(name)
-    name.eql?('All Clients') ? (t('views.common.' + name.parameterize.underscore)) : name.capitalize
+    name.eql?('All Clients') ? (t('views.common.' + name.parameterize.underscore)) : name.titleize
   end
 end
