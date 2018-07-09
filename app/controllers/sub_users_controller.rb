d0c7be4c4ebc0f0756d7118e48493114c3486daf
Wasif Saeed (2116) <wasif.saeed@nxb.com.pk>
2018-07-09 12:50:10 +0500

    UserCreate: Fixed user create email notification issue

--- app/controllers/sub_users_controller.rb
+++ app/controllers/sub_users_controller.rb
@@ -39,7 +39,7 @@ class SubUsersController < ApplicationController
         @sub_user.update(current_company: get_company_id)
         current_user.accounts.first.users << @sub_user
         begin
-          UserMailer.new_user_account(current_user, @sub_user).deliver if params[:notify_user]
+          UserMailer.new_user_account(current_user, @sub_user).deliver if params[:notify_user].to_i == 1
         rescue => e
           puts  e
         end
