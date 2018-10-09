49a3f6ac17e8c219cc51f6e4d03182bffcc5c0dd
Muhammad Tabish Saleem (01933) <tabish.saleem@nxb.com.pk>
2018-10-10 00:18:22 +0500

    ToastrJs: Integrated to for low priority alerts/notices and fixed styling

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,2 @@
-Rails.application.config.assets.precompile += %w( landing_page.css landing_page.js image.scss js/*)
+# Rails.application.config.assets.precompile += %w( landing_page.css landing_page.js image.scss js/*)
+Rails.application.config.assets.precompile += %w(toastr.min.css landing_page.css landing_page.js toastr.min.js image.scss js/*)
