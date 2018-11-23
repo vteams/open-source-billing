ec793bdc0bef9861a7d234b0fc3622fcbef1cfbb
Muhammad Tabish Saleem (01933) <tabish.saleem@nxb.com.pk>
2018-11-23 16:08:02 +0500

    ApplicationJS: Restricted user to inspect element via right click and F12 key

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -234,6 +234,16 @@ $(document).ready(function(){
 
     initSelectActionLink();
     initDemoLinksClick();
+
+    $("body").on("contextmenu",function(){
+        return false;
+    });
+
+    document.onkeydown = function(e) {
+        if(event.keyCode == 123) {
+            return false;
+        }
+    }
 });
 
 function initCustomConfirmPopUp() {
