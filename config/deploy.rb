3ee2904194da1c84e4b5dffd3374b7d5f6900d10
Muhammad Tabish Saleem (01933) <tabish.saleem@nxb.com.pk>
2019-01-07 13:13:21 +0500

    Deploy: Fixed paths and configurations issues for capistrano deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,19 +1,22 @@
 # config valid only for current version of Capistrano
-lock '3.6.1'
+# lock '3.6.1'
 
 set :application, 'osb'
-set :repo_url, 'git@gitlab.vteamslabs.com:vteams/v_billing.git'
+
+set :repo_url, 'git@github.com:vteams/open-source-billing.git'
 
 # Default branch is :master
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-#set :deploy_to, '/home/deploy/multi_tenants'
-
-set :deploy_to, '/home/osbstage/apps/multi_tenants'
+set :deploy_to, '/home/deploy/open-source-billing' # Directory path of remote server
 # Default value for :scm is :git
 # set :scm, :git
-set :rvm_ruby_version, '2.0.0-p648'
+
+set :rvm_ruby_version, '2.3.7'
+
+# To login into remote server
+server 'demo.opensourcebilling.com:22222', user: 'deploy', roles: %w{app db web}
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
 
@@ -23,15 +26,18 @@ set :rvm_ruby_version, '2.0.0-p648'
 
 # Default value for :pty is false
 # set :pty, true
-set :use_sudo,        false
+set :use_sudo, false
+
 # Default value for :linked_files is []
-append :linked_files, 'config/database.yml', 'config/config.yml', 'config/application.yml'
+append :linked_files, 'config/database.yml', 'config/config.yml'
 
 # Default value for linked_dirs is []
 # append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
-set :rails_env, fetch(:stage)
+
+set :rails_env, 'production'
+
 # Default value for keep_releases is 5
 set :keep_releases, 5
