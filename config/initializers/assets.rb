add5181abd2f91a4de439a627022a7bf1822d5a7
Muhammad Tabish Saleem (01933) <tabish.saleem@nxb.com.pk>
2019-01-07 21:16:13 +0500

    Required materialize.css and main.scss into assets.rb and removed from pdf layout

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,1 @@
-# Rails.application.config.assets.precompile += %w( landing_page.css landing_page.js image.scss js/*)
-Rails.application.config.assets.precompile += %w(toastr.min.css landing_page.css landing_page.js toastr.min.js image.scss js/*)
+Rails.application.config.assets.precompile += %w(toastr.min.css landing_page.css landing_page.js toastr.min.js image.scss pdf.css.scss js/*)
