2a4afe45ce543f0668e8d9144f6cfee6076a1f31
Muhammad Tabish Saleem (01933) <tabish.saleem@nxb.com.pk>
2019-01-16 19:41:11 +0500

    Puma.rb: Updated configurations

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,15 @@
 workers 2
-threads_count = 5
-threads threads_count, threads_count
+threads 4, 16
+port 9191
 
+app_dir = File.expand_path("../..", _FILE_)
+shared_dir = "#{app_dir}/shared"
+
+# Default to production
+rails_env = ENV['RAILS_ENV'] || "production"
+environment rails_env
+
+pidfile "#{shared_dir}/tmp/pids/puma.pid"
 preload_app!
 
 on_worker_boot do
@@ -9,3 +17,14 @@ on_worker_boot do
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
   ActiveRecord::Base.establish_connection
 end
+
+# Set up socket location
+# bind "unix:///home/deploy/shared/sockets/single_tenant.sock"
+#
+# # Logging
+# stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+#
+# # Set master PID and state locations
+# pidfile "/home/deploy/single_tenant/shared/tmp/pids/puma.pid"
+# state_path "/home/deploy/shared/tmp/pids/puma.state"
+# activate_control_app
