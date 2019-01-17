9cfd70dbf9b4b2d27c90ce2f62128e99dc1bf535
Muhammad Tabish Saleem (01933) <tabish.saleem@nxb.com.pk>
2019-01-17 20:58:02 +0500

    Deploy: Added start, stop and restart tasks in deploy script for Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,22 +5,41 @@
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 set :rvm_ruby_version, '2.3.7'
-
+set :rvm_binary, '/usr/local/rvm/bin/rvm'
 set :use_sudo, false
 
 # Default value for :linked_files is []
 append :linked_files, 'config/database.yml', 'config/config.yml', 'Gemfile.lock'
 
 # Default value for linked_dirs is []
-# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+append :linked_dirs, 'tmp/pids'#,'log', 'tmp/cache', 'tmp/sockets', 'public/system'
 
 # Default value for keep_releases is 5
 set :keep_releases, 5
 
+set :default_shell, '/bin/bash -l'
+
 namespace 'deploy' do
+  desc "Start the application"
+  task :start do
+    on roles(:app) do
+      execute "cd #{current_path} && RAILS_ENV=production #{fetch(:rvm_binary)} #{fetch(:rvm_ruby_version)} do bundle exec puma -p 9191 -d"
+    end
+  end
+
+  desc "Stop the application"
+  task :stop do
+    on roles(:app) do
+      execute "cd #{current_path} && RAILS_ENV=production #{fetch(:rvm_binary)} #{fetch(:rvm_ruby_version)} do bundle exec pumactl -P tmp/pids/puma.pid stop"
+    end
+  end
+
+  desc "Restart the application"
   task :restart do
-    "bundle exec puma -p 9191 -e production -d 'cat /home/deploy/single_tenant/shared/config/puma.pid' restart"
+    on roles(:app) do
+      execute "cd #{current_path} && RAILS_ENV=production #{fetch(:rvm_binary)} #{fetch(:rvm_ruby_version)} do bundle exec pumactl -P tmp/pids/puma.pid restart"
+    end
   end
 end
 
-after 'deploy:restart'
+after 'deploy:finishing', 'deploy:restart'
