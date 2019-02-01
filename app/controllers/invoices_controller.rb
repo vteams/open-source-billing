bc73920208e2f92de827005e68349436c34f1d8e
Muhammad Tabish Saleem (01933) <tabish.saleem@nxb.com.pk>
2019-02-01 21:05:43 +0500

    Invoice: Added sorting for table view and fixed sorting query

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -33,7 +33,7 @@ class InvoicesController < ApplicationController
   def index
     params[:status] = params[:status] || 'active'
     @status = params[:status]
-    @invoices = Invoice.joins("LEFT OUTER JOIN clients ON clients.id = invoices.client_id ").filter(params,@per_page).order("invoices.#{sort_column} #{sort_direction}")
+    @invoices = Invoice.joins("LEFT OUTER JOIN clients ON clients.id = invoices.client_id ").filter(params,@per_page).order("#{sort_column} #{sort_direction}")
     @invoices = filter_by_company(@invoices)
     @invoice_activity = Reporting::InvoiceActivity.get_recent_activity(get_company_id, @per_page, params.deep_dup)
     respond_to do |format|
