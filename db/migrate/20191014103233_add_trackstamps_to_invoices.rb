0824a6d5feabeef422e13a72600490ce9dcf6ed4
shahrozashraf <shahroz.ashraf@nxb.com.pk>
2019-10-21 12:26:08 +0500

    removed invoices trackstamps migration

--- db/migrate/20191014103233_add_trackstamps_to_invoices.rb
+++
@@ -1,6 +0,0 @@
-class AddTrackstampsToInvoices < ActiveRecord::Migration
-  def change
-        add_column :invoices, :created_by, :integer
-    add_column :invoices, :updated_by, :integer
-  end
-end
