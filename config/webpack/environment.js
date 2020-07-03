1567288fdc3a7555af4dd7d918fe843a44c50673
shahrozashraf <shahroz.ashraf@nxb.com.pk>
2020-07-03 14:31:57 +0500

    Webpacker integration

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,5 @@
 var webpack = require('webpack');
+const path = require('path');
 const { environment } = require('@rails/webpacker')
 const erb = require('./loaders/erb')
 const coffee =  require('./loaders/coffee')
@@ -7,7 +8,8 @@ environment.loaders.prepend('erb', erb)
 environment.loaders.append('coffee', coffee)
 environment.loaders.prepend('erb', erb)
 module.exports = environment
-environment.plugins.append('Provide', new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery'
+environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
+    $: "jquery",
+    JQuery: "jquery",
+    jquery: "jquery"
 }))
