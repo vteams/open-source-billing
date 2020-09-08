d335e63591e3921b83a14e94a29f15cc91bb5836
shahrozashraf <shahroz.ashraf@nxb.com.pk>
2020-09-08 14:39:15 +0500

    revert header changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -229,6 +229,32 @@ $(document).on('click', '.j-store-select', function() {
 
 
 $(document).ready(function(){
+    $.fn.textWidth = function(text, font) {
+
+        if (!$.fn.textWidth.fakeEl) $.fn.textWidth.fakeEl = $('<span>').hide().appendTo(document.body);
+
+        $.fn.textWidth.fakeEl.text(text || this.val() || this.text() || this.attr('placeholder')).css('font', font || this.css('font'));
+
+        return $.fn.textWidth.fakeEl.width();
+    };
+
+    $('.select-dropdown').on('input', function() {
+        var inputWidth = $(this).textWidth();
+        $(this).css({
+            width: inputWidth
+        });
+    }).trigger('input');
+
+
+    function inputWidth(elem, minW, maxW) {
+        elem = $(this);
+        console.log(elem);
+    }
+
+    var targetElem = $('.select-dropdown');
+
+    inputWidth(targetElem);
+
     $('.company_name').on('click', function (){
         $('.company_select .select-dropdown:first').show().focus().click();
     })
