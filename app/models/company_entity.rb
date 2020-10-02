f698112ca7b36ffa68f87b11bb67d173306ec38f
shahrozashraf <shahroz.ashraf@nxb.com.pk>
2020-10-02 16:48:48 +0500

    returned company_ids in clients and items list with rabl

--- app/models/company_entity.rb
+++ app/models/company_entity.rb
@@ -1,4 +1,6 @@
 class CompanyEntity < ActiveRecord::Base
   belongs_to :entity, :polymorphic => true
   belongs_to :parent, :polymorphic => true
+
+  scope :company_ids, -> (entity_id, entity_type) { where(entity_id: entity_id, entity_type: entity_type).pluck(:parent_id) }
 end
