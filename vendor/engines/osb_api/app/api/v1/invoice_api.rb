37507c920cdd645fc034ffb81f683409b9e27229
shahrozashraf <shahroz.ashraf@nxb.com.pk>
2021-02-18 17:48:25 +0500

    Created APIs to permanently delete invoices and recover archived and deleted invoices

--- vendor/engines/osb_api/app/api/v1/invoice_api.rb
+++ vendor/engines/osb_api/app/api/v1/invoice_api.rb
@@ -142,6 +142,43 @@ module V1
 
       end
 
+      desc 'Delete invoices permanently',
+           headers: {
+             "Access-Token" => {
+               description: "Validates your identity",
+               required: true
+             }
+           }
+      post 'delete_permanently' do
+        @invoices = Invoice.with_deleted.where(id: JSON.parse(params[:invoice_ids]))
+        if @invoices.present? && @invoices.any? {|i| i.deleted_at.nil?}
+          {error: "Active or Archived invoices cannot be deleted permanently"}
+        elsif @invoices.present? && @invoices.map(&:really_destroy!)
+          {message: "Invoice(s) deleted permanently"}
+        else
+          {error: "No Invoice found"}
+        end
+      end
+
+      desc 'Delete invoices permanently',
+           headers: {
+             "Access-Token" => {
+               description: "Validates your identity",
+               required: true
+             }
+           }
+      post 'bulk_actions' do
+        @invoices = Invoice.with_deleted.where(id: JSON.parse(params[:invoice_ids]))
+        actions = {recover_archived: 'unarchive', recover_deleted: "restore"}
+        if @invoices.present?
+          @invoices.map(&actions[params[:action].to_sym].to_sym)
+          {error: "Invoice(s) recovered successfully"}
+        else
+          {error: "No Invoice found"}
+        end
+      end
+
+
       desc "Send invoice to client",
            headers: {
                "Access-Token" => {
