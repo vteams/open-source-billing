d4db00ace2a5ab387f63ca18ba5d2249b38a5dea
Muhammad Hamza <muhammad.hamza@nxb.com.pk>
2026-03-27 12:55:12 +0500

    Resolved webpacker issue for staging

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :rvm_binary, '/usr/local/rvm/bin/rvm'
 set :use_sudo, false
 
 # Default value for :linked_files is []
-append :linked_files, 'config/database.yml', 'config/config.yml', 'Gemfile.lock'
+append :linked_files, 'config/database.yml', 'config/config.yml', 'Gemfile.lock', 'config/secrets.yml'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'tmp/pids'#,'log', 'tmp/cache', 'tmp/sockets', 'public/system'
@@ -40,6 +40,16 @@ namespace 'deploy' do
       execute "cd #{current_path} && RAILS_ENV=production #{fetch(:rvm_binary)} #{fetch(:rvm_ruby_version)} do bundle exec pumactl -P tmp/pids/puma.pid restart"
     end
   end
-end
 
+  desc 'Install JS dependencies with Yarn'
+  task :yarn_install do
+    on roles(:web) do
+      within release_path do
+        # Adjust this if Yarn is installed via NVM or custom path
+        execute "/home/dev/.nvm/versions/node/v18.20.8/bin/yarn install --silent --no-progress"
+      end
+    end
+  end
+end
+before 'deploy:assets:precompile', 'deploy:yarn_install'
 after 'deploy:finishing', 'deploy:restart'
